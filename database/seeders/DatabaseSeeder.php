ef88924bc983b58c09242b25cc308d2b0fdbd8e6
nadia.trfm <69869414+Nadia-ru@users.noreply.github.com>
2023-07-31 16:33:05 +0300

    seeds

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class DatabaseSeeder extends Seeder
 {
@@ -18,5 +19,44 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+
+
+
+        DB::table('clients')->insert([
+            'full_name' => 'Надежда Трофимова Д',
+            'is_female' => 1,
+            'tel' => 222222,
+            'address' => 'Волгоград',
+        ]);
+        DB::table('clients')->insert([
+            'full_name' => 'Иван Иванов И',
+            'is_female' => 0,
+            'tel' => 222222,
+            'address' => 'Волгоград',
+        ]);
+        DB::table('clients')->insert([
+            'full_name' => 'Павел Иванов Л',
+            'is_female' => 0,
+            'tel' => 89001234567,
+            'address' => 'Саратов',
+        ]);
+        DB::table('clients')->insert([
+            'full_name' => 'Екатерина Петрова К',
+            'is_female' => 1,
+            'tel' => 89001234512,
+            'address' => 'Волгоград',
+        ]);
+        DB::table('clients')->insert([
+            'full_name' => 'Анастасия Павлова Н',
+            'is_female' => 1,
+            'tel' => 89001234589,
+            'address' => 'Волгоград',
+        ]);
+        DB::table('clients')->insert([
+            'full_name' => 'Наталья Светлая О',
+            'is_female' => 1,
+            'tel' => 89001234589,
+            'address' => 'Волгоград',
+        ]);
     }
 }
